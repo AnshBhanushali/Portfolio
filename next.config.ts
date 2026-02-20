f6337475c9cc36eb0974ed9ee346ad7f5245680b
Ansh Bhanushali <bhanusad@mail.uc.edu>
2026-02-19 19:42:04 -0500

    Fix next.config syntax

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,14 @@
 // next.config.ts
-import type { NextConfig } from 'next';
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ["images.unsplash.com", "upload.wikimedia.org"],
-  },
-  eslint: {
-    ignoreDuringBuilds: true, 
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "example.com",
+      },
+    ],
   },
 };
 
